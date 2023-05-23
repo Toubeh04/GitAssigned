fd3590c7c88eadce481e7031206e6b96d8549860
Toubsh04 <ibraheemaltoubeh@hotmail.com>
2023-05-24 00:33:09 +0300

    hello world

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!!");
+        // done by Ibrahim
+        System.out.println("atypon training");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your name:");
+        String name = scanner.nextLine();
+        System.out.println("Your name is: "+name);
+
     }
 }
