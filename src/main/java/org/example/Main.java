ad4c7a0be10f32b70946c369c639eb162d38561a
Toubsh04 <ibraheemaltoubeh@hotmail.com>
2023-05-24 00:09:00 +0300

    changed the main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Done by Ibrahim
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your height (in cm): ");
+        double height = Double.parseDouble(scanner.nextLine());
+        System.out.println("Enter your favourite sport: ");
+        String favSport = scanner.nextLine();
+
+
     }
 }
